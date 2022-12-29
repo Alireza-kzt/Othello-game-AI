9786755f6963430b9b839387fe6df8e150f7e525
Amin <Xporo720@gmail.com>
2022-12-29 05:44:03 -0800

    create class state

--- model.py
+++ model.py
@@ -46,3 +46,71 @@ class Disk:
 
     def __hash__(self) -> int:
         return hash(self.x) + hash(self.convertor[self.y]) +  hash(self.x)*hash(self.convertor[self.y]) + hash(self.x*self.convertor[self.y]) + hash(str(self.x)+self.convertor[self.y])
+
+
+class State:
+    """
+    - State
+    contain location of disk white and black , depth of state , turn of player and parent state
+    default usage create root state Othello - Official Board Game
+    usage State(state) return copy of object state
+    """
+    player_white : list[Disk]  # list of position disk white
+    player_black : list[Disk]  # list of position disk black
+    depth : int # depth state
+    turn : bool # true : white , false : black
+    parent : 'State' # parent of this state
+
+    def __init__(self) -> None:
+        self.player_white = [Disk(3,3),Disk(4,4)]
+        self.player_black = [Disk(3,4),Disk(4,3)]
+        self.depth = 0
+        self.turn = True
+        self.parent = None
+
+    def __init__(self,parent) -> None:
+        self.player_white = parent.player_white
+        self.player_black = parent.player_black
+        self.depth = parent.depth
+        self.turn = parent.turn
+        self.parent = parent
+
+
+
+    """
+    disk  := new disk for player turn
+        type object Disk 
+    disks :=  list disk must be change
+    """
+    def add_disk(self, disk, disks: list[Disk]):
+        if self.turn:
+            self.player_white.append(disk)
+            for disk in disks:
+                self.player_black.remove(disk)
+                self.player_white.append(disk)
+        else:
+            self.player_black.append(disk)
+            for disk in disks:
+                self.player_white.remove(disk)
+                self.player_black.append(disk)
+        self.depth += 1
+        self.turn = not self.turn
+        if not self.valid_move():
+            self.turn = not self.turn
+
+
+    def __hash__(self) -> int:
+        h = 0
+        for disk in self.player_white:
+            h += hash(disk)*hash("white")*hash(str(self.turn))
+        for disk in self.player_black:
+            h += hash(disk)*hash("black")*hash(str(self.turn))
+        return h
+
+
+    def valid_move(self):
+        pass
+
+
+
+
