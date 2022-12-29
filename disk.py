0c69f67d7735091a131e0fd73d55bbafab980ce3
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2022-12-29 21:55:39 +0330

    refactor

---
+++ disk.py
@@ -0,0 +1,48 @@
+class Disk:
+    """
+    - disk
+        contain location of disk in Othello Game
+        x := row
+        y := column
+        convertor :=
+            f : X -> X , function 1-1 and Onto
+            X := {0,1,2,3,4,5,6,7} union {a,b,c,d,e,f,g,h}
+
+    - hash calculation
+        hash(x) + hash(f(y)) + hash(x)*hash(f(y)) + hash(x*f(y)) + hash(x+f(y))
+      example
+        x = 3
+        y = 3 -> f(y) = 'd'
+        hash(3) + hash('d') + hash(3)*hash('d') + hash('ddd') + hash('3d')
+    """
+    x: int
+    y: int
+
+    convertor = {
+        0: "a",
+        1: "b",
+        2: "c",
+        3: "d",
+        4: "e",
+        5: "f",
+        6: "g",
+        7: "h",
+        "a": 0,
+        "b": 1,
+        "c": 2,
+        "d": 3,
+        "e": 4,
+        "f": 5,
+        "g": 6,
+        "h": 7,
+    }
+
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+    def __hash__(self) -> int:
+        return hash(self.x) + hash(self.convertor[self.y]) + hash(self.x) * hash(self.convertor[self.y]) + hash(
+            self.x * self.convertor[self.y]) + hash(str(self.x) + self.convertor[self.y])
+
+
---
+++ state.py
@@ -0,0 +1,70 @@
+from disk import Disk
+
+
+class State:
+    """
+    - State
+    contain location of disk white and black , depth of state , turn of player and parent state
+    default usage create root state Othello - Official Board Game
+    usage State(state) return copy of object state
+    """
+    white_player: list[Disk]  # list of position disk white
+    black_player: list[Disk]  # list of position disk black
+    depth: int  # depth state
+    turn: bool  # true : white , false : black
+    parent: 'State'  # parent of this state
+
+    def __init__(self) -> None:
+        self.white_player = [Disk(3, 3), Disk(4, 4)]
+        self.black_player = [Disk(3, 4), Disk(4, 3)]
+        self.depth = 0
+        self.turn = True
+        self.parent = None
+
+    def __init__(self, parent) -> None:
+        self.white_player = parent.white_player
+        self.black_player = parent.black_player
+        self.depth = parent.depth
+        self.turn = parent.turn
+        self.parent = parent
+
+    """
+    disk  := new disk for player turn
+        type object Disk 
+    disks :=  list disk must be change
+    """
+
+    def add_disk(self, disk, disks: list[Disk]) -> None:
+        if self.turn:
+            self.white_player.append(disk)
+            for disk in disks:
+                self.black_player.remove(disk)
+                self.white_player.append(disk)
+        else:
+            self.black_player.append(disk)
+            for disk in disks:
+                self.white_player.remove(disk)
+                self.black_player.append(disk)
+        self.depth += 1
+        self.turn = not self.turn
+        if not self.valid_move():
+            self.turn = not self.turn
+
+    def __hash__(self) -> int:
+        h = 0
+        for disk in self.white_player:
+            h += hash(disk) * hash("white") * hash(str(self.turn))
+        for disk in self.black_player:
+            h += hash(disk) * hash("black") * hash(str(self.turn))
+        return h
+
+    def successor(self) -> list['State']:
+        pass
+
+    def valid_move(self) -> bool:
+        """
+            - valid_move
+            return true if player turn can move
+            return false if player turn can not move
+            """
+        return self.successor() != []
