d8fe31c0ca5a23198fb8f3b9692834fcc3d6aeea
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2022-12-31 01:30:24 +0330

    minmax algorithm

--- othello.py
+++ othello.py
@@ -1,3 +1,25 @@
+from state import State
+
+
 class OthelloAI:
-    def minmax(self):
-        pass
+    def minmax(self, state: State, cutoff: int, current_level=0) -> State:
+        if current_level == cutoff:
+            return state
+
+        nodes = state.successor()
+
+        if len(nodes) == 0:
+            return state
+
+        if state.turn:
+            max(
+                [
+                    self.minmax(node, cutoff, current_level + 1) for node in nodes
+                ]
+            )
+        else:
+            min(
+                [
+                    self.minmax(node, cutoff, current_level + 1) for node in nodes
+                ]
+            )
