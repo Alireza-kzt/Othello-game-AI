9c6ef70505df303f97029a791ec7f01f44de23f4
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-01-04 09:07:52 +0330

    alpha beta minmax

--- othello.py
+++ othello.py
@@ -16,3 +16,25 @@ class OthelloAI:
                 self.minmax(node, cutoff, current_level + 1) for node in nodes
             ]
         )
+
+    def alpha_beta_minmax(self, state: State, cutoff=2, current_level=0, alpha=-1, beta=1):
+        if current_level == cutoff:
+            return state
+
+        nodes = state.successor()
+
+        if len(nodes) == 0:
+            return state
+
+        scores = []
+        for node in nodes:
+            scores.append(score := self.alpha_beta_minmax(node, cutoff, current_level, alpha, beta))
+
+            if state.turn:
+                alpha = max(alpha, score)
+            else:
+                beta = min(beta, score)
+            if beta <= alpha:
+                break
+
+        return (max if state.turn else min)(scores)
