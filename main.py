cdf66166f1838f55bba58c7a1906f3325f6b0338
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-01-08 18:23:49 +0330

    refactor

--- main.py
+++ main.py
@@ -10,33 +10,33 @@ if __name__ == '__main__':
     state = State()  # init state
 
     while not state.is_goal():
-        next_state = black_player.minmax(state) if state.turn else white_player.minmax(state)
+        next_state = black_player.minmax(state, state.turn) if state.turn else white_player.minmax(state, state.turn)
         state = next_state if state is not None else state
 
     print(state.get_winner())
 
-A = np.zeros([8, 8])
-state = State()
-c = {
-    True: 1,
-    False: -1,
-}
-t1 = datetime.datetime.now()
-for i in range(100):
-    if len(state.successor()[0]) != 0:
-        state = state.successor()[0].pop()
-        if not state.valid_move():
-            state.turn = not state.turn
-        A = np.zeros([8, 8])
-        for player, disks in state.players.items():
-            for disk in disks:
-                A[disk.x][disk.y] = c[player]
-        # print(state.turn)
-        # print(A)
-t2 = datetime.datetime.now()
-print(t2-t1)
-print(len(state.players[True]))
-print(len(state.players[False]))
+# A = np.zeros([8, 8])
+# state = State()
+# c = {
+#     True: 1,
+#     False: -1,
+# }
+# t1 = datetime.datetime.now()
+# for i in range(100):
+#     if len(state.successor()[0]) != 0:
+#         state = state.successor()[0].pop()
+#         if not state.valid_move():
+#             state.turn = not state.turn
+#         A = np.zeros([8, 8])
+#         for player, disks in state.players.items():
+#             for disk in disks:
+#                 A[disk.x][disk.y] = c[player]
+#         # print(state.turn)
+#         # print(A)
+# t2 = datetime.datetime.now()
+# print(t2-t1)
+# print(len(state.players[True]))
+# print(len(state.players[False]))
 
 
 
