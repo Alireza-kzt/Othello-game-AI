207dd0f8ec1532f8ad1d5a1c9e1da1610e8bb6fb
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-01-10 09:31:23 +0330

    refactor

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ if __name__ == '__main__':
     white_player = OthelloAI()
 
     state = State()  # init state
+    state.render()
 
     while not state.is_goal():
-        state.render()
         state = black_player.action(state, level=3) if state.turn else white_player.action(state, level=1)
+        state.render()
 
     black_score, white_score = state.get_scores()
-    print(r'Black player score: ', black_score)
-    print(r'White player score: ', white_score)
+    print('Black player score: ', black_score)
+    print('White player score: ', white_score)
